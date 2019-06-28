0798411b926e42b9f9a2516506121bd3033d1188
fioriecheyenne <fioriecheyenne@gmail.com>
2019-06-28 16:54:17 -0700

    Updates

---
+++ precache-manifest.2fd092ca40f2488ceadd41eb613ee988.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b316faf0c65fd09b6315204463ec950",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "b9722a894217a204201b",
+    "url": "/clicky-game/static/css/main.3cd56bf5.chunk.css"
+  },
+  {
+    "revision": "bb57689562246172007c",
+    "url": "/clicky-game/static/js/2.6445d66d.chunk.js"
+  },
+  {
+    "revision": "b9722a894217a204201b",
+    "url": "/clicky-game/static/js/main.5f613afd.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
